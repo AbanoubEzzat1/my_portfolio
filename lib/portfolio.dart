e290ec91bf41f8675db18c368ff61af4a5f43586
DESKTOP-VP4KMCV\AbanoubEzzat <abanoubezzat50@gmail.com>
2024-02-24 15:49:25 +0200

    Setup Project

---
+++ lib/portfolio.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:my_portfolio/features/home/ui/home_screen.dart';
+
+class Portfolio extends StatelessWidget {
+  const Portfolio({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Portfolio',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
