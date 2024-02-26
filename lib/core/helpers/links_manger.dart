843d4d884bc2d07026692193b86b5a0909f42fbc
DESKTOP-VP4KMCV\AbanoubEzzat <abanoubezzat50@gmail.com>
2024-02-26 12:06:43 +0200

    Continue developing the home screen

---
+++ lib/core/helpers/links_manger.dart
@@ -0,0 +1,7 @@
+class LinksManger {
+  static const String linkedInUrl =
+      'https://www.linkedin.com/in/abanoub-ezzat97/';
+  static const String gitHubUrl = 'https://github.com/AbanoubEzzat1';
+  static const String emileUrl = 'mailto:abanoubezzat50@gmail.com';
+  static const String phoneUrl = 'https://wa.me/01271739443';
+}
--- lib/core/widgets/text_button.dart
+++ lib/core/widgets/text_button.dart
@@ -3,16 +3,18 @@ import 'package:flutter/material.dart';
 class AppTextButton extends StatelessWidget {
   final String text;
   final TextStyle style;
+  final void Function()? onPressed;
   const AppTextButton({
     super.key,
     required this.text,
     required this.style,
+    this.onPressed,
   });
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
-      onPressed: () {},
+      onPressed: onPressed,
       child: Text(
         text,
         style: style,
